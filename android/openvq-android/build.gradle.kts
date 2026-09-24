11d4c25ece545ef532f48fd38623bf1731c8c66a
Abdullah X <ax.zephara@gmail.com>
2026-09-24 07:08:24 +0530

    build: pin Android NDK for reproducible JNI builds

--- android/openvq-android/build.gradle.kts
+++ android/openvq-android/build.gradle.kts
@@ -5,9 +5,15 @@ plugins {
 android {
     namespace = "ai.openvq"
     compileSdk = 35
+    ndkVersion = "27.0.12077973"
     defaultConfig {
         minSdk = 24
         externalNativeBuild { cmake { cppFlags += "-std=c++17" } }
     }
-    externalNativeBuild { cmake { path = file("src/main/cpp/CMakeLists.txt") } }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
 }
